bcfb3246ea12f1a7d24103653887f7dcf97d0c31
Matias Manoel Uchôa do Amaral <33668931+matiasmanoel@users.noreply.github.com>
2018-04-24 15:09:42 -0300

    Storage
    Pasta storage, que fica dentro da pasta geral fablab1

---
+++ storage/framework/views/9320378f95d9ff6da19ba3c4ffe0165fd77c32b0.php
@@ -0,0 +1,96 @@
+<!--
+Para funcionar o background geral com o background do app.blade, tem que colocar o comando abaixo nas abas que quer modificar o backgroud,
+e alterar o background no css também, só com os dois pra funcionar
+
+#1 - Alterei o "card-body" do app.css, adicionando um campo "background-color:#8998818c;" para que o login e o register compartilhassem de uma estética diferente, mas iguais para ambas.
+#2 - Alterei o "btn-primary" do app.css para "#215467" e o "btn:hover para "#1f323a""
+-->
+
+<!-- ###########ACHAR O LOCAL QUE INFORMA OS ERROS, POIS QUANDO O USUÁRIO ERRA O LOGIN, O ERRO QUE ESTÁ SENDO INFORMADO, AINDA ESTÁ EM INGLÊS########## -->
+<style>
+    html, body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+        background-image: url(https://wallpaperscraft.com/image/light_faded_background_85547_2560x1600.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+</style>
+
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Entrar')); ?></div>
+
+                <div class="card-body">
+                    <form method="POST" action="<?php echo e(route('login')); ?>">
+                        <?php echo csrf_field(); ?>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-4 col-form-label text-md-right"><?php echo e(__('Endereço de E-mail')); ?></label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control<?php echo e($errors->has('email') ? ' is-invalid' : ''); ?>" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
+
+                                <?php if($errors->has('email')): ?>
+                                    <span class="invalid-feedback">
+                                        <strong><?php echo e($errors->first('email')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right"><?php echo e(__('Senha')); ?></label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control<?php echo e($errors->has('password') ? ' is-invalid' : ''); ?>" name="password" required>
+
+                                <?php if($errors->has('password')): ?>
+                                    <span class="invalid-feedback">
+                                        <strong><?php echo e($errors->first('password')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>> <?php echo e(__('Lembrar-me!')); ?>
+
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <?php echo e(__('Entrar')); ?>
+
+                                </button>
+
+                                <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>">
+                                    <?php echo e(__('Esqueceu sua Senha?')); ?>
+
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
